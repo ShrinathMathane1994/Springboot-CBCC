38c94a8873102a96ba5a94d93680d4c2eb84dd8b
ShrinathMathane1994 <shreenath.mathane@gmail.com>
2025-09-10 17:34:16 +0530

    Update TestCaseRunHistoryRepository.java

--- src/main/java/com/qa/cbcc/repository/TestCaseRunHistoryRepository.java
+++ src/main/java/com/qa/cbcc/repository/TestCaseRunHistoryRepository.java
@@ -2,6 +2,9 @@ package com.qa.cbcc.repository;
 
 import com.qa.cbcc.model.TestCaseRunHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TestCaseRunHistoryRepository extends JpaRepository<TestCaseRunHistory, Long> {
 
